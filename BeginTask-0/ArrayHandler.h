99efc1ed398345ab77dbcf234a1a18d5354084ec
unknown <vovamoskaev6@gmail.com>
2024-10-07 14:56:27 +0300

    134

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,16 +1,40 @@
-
+//Andrey Chertilla
 template<typename T>
 class ArrayHandler {
 private:
     size_t _size;
     T* _array;
+    size_t _min;
+    size_t _max;
+    size_t count;
 public:
     ArrayHandler(size_t size = 10) {
-
+        _size = size;
+        _array = new T[_size];
+        _max = 0;
+        _min = 100000001;
+        count = 0;
     }
 
     void AppendElem(T elem) {
+        if (count == _size){
+            _size *= 10; 
+            T* array_2 = new T [_size];
+            for (size_t i = 0; i < count; i ++){
+                array_2[i] = _array[i];
+            }
+            delete [] _array;
+            _array = array_2;
+        }
+        _array[count] = elem;
+        count ++;
+        if (_min > elem){
+            _min = elem;
+        }
 
+        if (_max < elem){
+            _max = elem;
+        }
     }
 
     bool IsContains(T elem) {
@@ -18,15 +42,15 @@ public:
     }
 
     T GetMax() {
-        return 0;
+        return _max;
     }
 
     T GetMin() {
-        return 0;
+        return _min;
     }
 
     ~ArrayHandler() {
-
+        delete [] _array;
     }
 
 };
